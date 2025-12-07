11ac37bf16f63058ce770dc99338667b0a1afa73
ct2cx <ct2cx@protonmail.com>
2025-12-07 20:54:40 +0600

    after/plugin: change highlighting group of `NormalFloat`

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_augroup('AfterPlugin', { clear = false })
 vim.api.nvim_create_autocmd('ColorScheme', {
 	pattern = '*',
 	callback = function(args)
-		vim.api.nvim_set_hl(0, 'NormalFloat', { bg = funcs:get_hl('BufferVisible', 'bg') })
+		vim.api.nvim_set_hl(0, 'NormalFloat', { bg = funcs:get_hl('StatusLine', 'bg') })
 		vim.api.nvim_set_hl(0, 'NvimTreeCursorLine', { link = 'CursorLine' })
 		vim.api.nvim_set_hl(0, 'NvimTreeWinSeparator', { fg = funcs:get_hl('Normal', 'bg') })
 		vim.api.nvim_set_hl(0, 'BufferLineBackground', { fg = funcs:get_hl('StatuslineNC', 'fg'), bg = funcs:get_hl('Statusline', 'bg') })
