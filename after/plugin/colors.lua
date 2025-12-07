6962054a079db4b2beedb7bc98773a86f904a178
ct2cx <ct2cx@protonmail.com>
2025-12-07 20:00:17 +0600

    after/plugin: add `colors.lua` for global color modifications

---
+++ after/plugin/colors.lua
@@ -0,0 +1,31 @@
+local funcs = require('funcs')
+
+-- Modification that would apply globally for any colorschemes
+vim.api.nvim_create_augroup('CustomColors', { clear = true })
+vim.api.nvim_create_autocmd('ColorScheme', {
+	pattern = '*',
+	callback = function(args)
+		vim.api.nvim_set_hl(0, 'NormalFloat', { bg = funcs:get_hl('BufferVisible', 'bg') })
+		vim.api.nvim_set_hl(0, 'NvimTreeCursorLine', { link = 'CursorLine' })
+		vim.api.nvim_set_hl(0, 'NvimTreeWinSeparator', { fg = funcs:get_hl('Normal', 'bg') })
+		vim.api.nvim_set_hl(0, 'BufferLineBackground', { fg = funcs:get_hl('StatuslineNC', 'fg'), bg = funcs:get_hl('Statusline', 'bg') })
+		vim.api.nvim_set_hl(0, 'BufferLineBufferSelected', { link = 'Normal' })
+		vim.api.nvim_set_hl(0, 'BufferLineBufferVisible', { link = 'Normal' })
+		vim.api.nvim_set_hl(0, 'BufferLineFill', { link = 'Statusline' })
+		vim.api.nvim_set_hl(0, 'BufferLineOffsetSeparator', { fg = funcs:get_hl('Normal', 'bg'), bg = funcs:get_hl('Normal', 'bg') })
+		vim.api.nvim_set_hl(0, 'BufferLineIndicatorSelected', { link = 'BufferLineOffsetSeparator' })
+		vim.api.nvim_set_hl(0, 'BufferLineIndicatorVisible', { link = 'BufferLineOffsetSeparator' })
+		vim.api.nvim_set_hl(0, 'BufferLineTab', { fg = funcs:get_hl('Normal', 'fg'), bg = funcs:get_hl('Visual', 'bg') })
+		vim.api.nvim_set_hl(0, 'BufferLineTabSelected', { fg = funcs:get_hl('Normal', 'bg'), bg = funcs:get_hl('DiagnosticInfo', 'fg') })
+		vim.api.nvim_set_hl(0, 'BufferLineTabClose', { bg = funcs:get_hl('DiagnosticError', 'fg'), fg = funcs:get_hl('Normal', 'bg') })
+		vim.api.nvim_set_hl(0, 'BufferLineDuplicate', { link = 'BufferLineBackground' })
+		vim.api.nvim_set_hl(0, 'BufferLineModified', { link = 'BufferLineBackground' })
+		vim.api.nvim_set_hl(0, 'BufferLineModifiedSelected', { link = 'Normal' })
+		vim.api.nvim_set_hl(0, 'BufferLineModifiedVisible', { link = 'Normal' })
+		vim.api.nvim_set_hl(0, 'BufferLineDuplicateVisible', { link = 'BufferLineDuplicateSelected' })
+	end,
+	group = 'CustomColors'
+})
+
+-- Specify you preferred colorscheme
+vim.cmd.colorscheme 'catppuccin'
