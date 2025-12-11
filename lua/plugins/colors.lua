69c0d96126c2df79eb8c858a45c495fdb9b7822a
ct2cx <ct2cx@protonmail.com>
2025-12-11 20:17:44 +0600

    plugins: disable italic style for all colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,17 @@
 return {
 	{
 		'rose-pine/neovim', name = 'rose-pine', priority = 1000,
+		opts = {
+			styles = {
+				italic = false
+			}
+		}
 	},
 	{
 		'catppuccin/nvim', name = 'catppuccin', priority = 1000,
+		opts = {
+			no_italic = true
+		}
 	},
 	{
 		'sainnhe/gruvbox-material',
@@ -12,7 +20,8 @@ return {
 		config = function()
 			-- vim.g.gruvbox_material_foreground = 'original'
 			-- vim.g.gruvbox_material_background = 'hard'
-			vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_enable_italic = 0
+			vim.g.gruvbox_material_disable_italic_comment = 1
 			-- vim.g.gruvbox_material_enable_bold = 1
 			-- vim.g.gruvbox_material_transparent_background = 1
 		end
