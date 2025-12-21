978ed1b33db4ff2716ada30edcef1b0dd78d2c75
ct2cx <ct2cx@protonmail.com>
2025-12-22 03:01:12 +0600

    plugins: use italic and bold fonts for gruvbox-material theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -12,10 +12,10 @@ return {
 		config = function()
 			-- vim.g.gruvbox_material_foreground = 'original'
 			-- vim.g.gruvbox_material_background = 'hard'
-			-- vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_enable_italic = 1
 			-- vim.g.gruvbox_material_disable_italic_comment = 1
 			vim.g.gruvbox_material_enable_italic = 1
-			-- vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_enable_bold = 1
 			-- vim.g.gruvbox_material_transparent_background = 1
 		end
 	},
