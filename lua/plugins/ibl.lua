02557358a4b958b75d1b898ff5036a82cb5144fc
ct2cx <ct2cx@protonmail.com>
2026-01-20 21:35:06 +0600

    plugins: add indent-blankline

---
+++ lua/plugins/ibl.lua
@@ -0,0 +1,15 @@
+return {
+	'lukas-reineke/indent-blankline.nvim',
+	config = function()
+		require('ibl').setup {
+			indent = {
+				char = '│'
+			},
+			scope = {
+				show_start = false,
+				show_end = false,
+				highlight = 'LineNr'
+			}
+		}
+	end
+}
